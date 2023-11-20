56bce23deb31feecdd0823d4c8c40492ee205ab2
DorinVieru <dorino.vieru@gmail.com>
2023-11-20 10:07:41 +0100

    [UPDATE] Complete exercise 4/b

--- js/script.js
+++ js/script.js
@@ -1,21 +1,30 @@
+// ESERCIZIO 1
 let number = 100;
 console.log(number);
 
+// ESERCIZIO 2
 const pi = 3.14;
 console.log(pi);
 
+// ESERCIZIO 2/B
 // ERRORE
 // pi = 31416
 // console.log(pi)
 
+// ESERCIZIO 2/C
 let radius = 8;
 let circle = radius * 2 * pi;
 console.log(circle);
 
+// ESERCIZIO 3
 let name = "Carlo";
 name = "Marco";
 console.log(name);
 
+// ESERCIZIO 4
 let greet = "Ciao Mondo!";
 console.log(greet);
 
+// ESERCIZIO 4/B
+let greet = "Come stai, Mondo?"
+console.group(greet);
